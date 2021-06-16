d57d1b882812bf065604bb4474753ca17f9a6786
grhamm-pabst <grhammpabst456@gmail.com>
2021-06-16 14:57:42 -0300

    Consertado relações do user cocm certificate

--- lib/devspot/certificates/certificate.ex
+++ lib/devspot/certificates/certificate.ex
@@ -2,14 +2,17 @@ defmodule Devspot.Certificate do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias Devspot.User
+
   @derive {Jason.Encoder, only: [:user_id, :url, :title]}
 
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
   schema "certificates" do
     field :url, :string
-    field :user_id, :binary_id
     field :title, :string
+
+    belongs_to :user, User
 
     timestamps()
   end

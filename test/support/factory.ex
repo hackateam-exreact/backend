3508421e3df6b58ee4537f32018a0386a96e1647
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2021-06-17 22:50:30 -0300

    adiciona experiences

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -1,7 +1,7 @@
 defmodule Devspot.Factory do
   use ExMachina.Ecto, repo: Devspot.Repo
 
-  alias Devspot.User
+  alias Devspot.{Experience, User}
 
   def user_params_factory do
     %{
@@ -34,6 +34,26 @@ defmodule Devspot.Factory do
       "user_id" => "b721fcad-e6e8-4e8f-910b-6911f2158b4a",
       "url" => "https://balta.io/certificados/1fd6a983-6805-4bb6-8cbd-274e5364d9db",
       "title" => "Começando com Angular com carga horária de 2 horas"
+    }
+  end
+
+  def experience_params_factory do
+    %{
+      "user_id" => "b721fcad-e6e8-4e8f-910b-6911f2158b4a",
+      "company" => "Stone",
+      "role" => "Elixir Backend Developer",
+      "start" => "30/11/2011",
+      "end" => "30/12/2012"
+    }
+  end
+
+  def experience_factory do
+    %Experience{
+      user_id: "b721fcad-e6e8-4e8f-910b-6911f2158b4a",
+      company: "Stone",
+      role: "Elixir Backend Developer",
+      start: ~D[2011-11-30],
+      end: ~D[2012-12-30]
     }
   end
 end
